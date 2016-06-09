1502126432c022be14a6144441e6493291740bc2
Jared Burrows <jaredsburrows@gmail.com>
2016-06-08 19:56:48 -0700

    Gradle updates

--- ios-gradle-objc-template/include/AppDelegate.h
+++ ios-gradle-objc-template/include/AppDelegate.h
@@ -10,8 +10,23 @@
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
+/*!
+ * @discussion A really simple way to calculate the sum of two numbers.
+ * @return The sum of the two numbers passed in.
+ */
 @property (strong, nonatomic) UIWindow *window;
 
+/*!
+ * @brief A Title for the car. Make it funny. Seriously.
+ */
+@property (nonatomic) AppDelegate *window2;
+
+/*!
+ @brief It converts temperature degrees from Fahrenheit to Celsius scale.
+ @discussion asd
+ @param  fromFahrenheit The input value representing the degrees in the Fahrenheit scale.
+ @return float The degrees in the Celsius scale.
+ */
+-(float)toCelcius:(float)fromFahrenheit;
 
 @end
-
